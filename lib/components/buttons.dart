ce505f320d9e9e9055313a1f8c2ba925d4c13c30
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-08-07 01:23:13 +0530

    minor adjustments

--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -1,11 +1,12 @@
-// ignore_for_file: prefer_const_constructors, must_be_immutable
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
 class AddTaskButton extends StatelessWidget {
   final String buttonLabel;
-  VoidCallback onPressed;
+  final VoidCallback onPressed;
 
+  // ignore: prefer_const_constructors_in_immutables
   AddTaskButton({
     super.key, 
     required this.buttonLabel,
